d9bfc5e5ac1b294d765953516c5fbd5a027bcd64
lee <shanleebaldonado24@gmail.com>
2026-08-06 23:23:40 +0800

    Add files via upload

---
+++ HelloWorld/public class Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String favPet = "Dog";
+        String name= "Alliah Julia Somido / Ley";
+        String birthday = "07/08/2007";
+        String address = "Baclayan, New Lucena, Iloilo";
+        String favSong = "Perfect by Ed Sheeran";
+        String motivation = "To live my dream life.";
+        String support = "Deadlines are reasonable.";
+        
+        System.out.println("Fav Pet    : " + favPet);
+        System.out.println("Name       : " + name);
+        System.out.println("Birthday   : " + birthday);
+        System.out.println("Address    : " + address);
+        System.out.println("Fav Song   : " + favSong);
+        System.out.println("Motivation : " + motivation);
+        System.out.println("Support    : " + support);
+    }
+}
