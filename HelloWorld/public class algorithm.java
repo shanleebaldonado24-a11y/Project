d439a79bbb51c07d55621f1d8bfec793dc92311a
lee <shanleebaldonado24@gmail.com>
2026-08-06 23:27:49 +0800

    Add files via upload

---
+++ HelloWorld/public class algorithm.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double distance = 100.0;
+        double time = 2.0;
+
+        double speed = distance / time;
+
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
