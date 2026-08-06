cde8193bc7482fc92d69d093aa9e117da0aa9881
lee <shanleebaldonado24@gmail.com>
2026-08-06 23:26:50 +0800

    Add files via upload

---
+++ HelloWorld/public class Total Calculator.java
@@ -0,0 +1,11 @@
+public class Total Calculator 
+{ 
+    public static void main(String[] args) 
+    {
+        int x=80;
+        int y=21;
+
+        int total = x + y;
+        System.out.println("Total: " + total);
+    }
+} 
